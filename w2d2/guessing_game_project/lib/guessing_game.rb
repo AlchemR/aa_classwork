abb6d4b5d3175b084a3181a7cf6c3be12dcf620d
AlchemR <102183958+AlchemR@users.noreply.github.com>
2022-04-26 09:01:24 -0700

    inital commit

---
+++ w2d2/guessing_game_project/lib/guessing_game.rb
@@ -0,0 +1,51 @@
+class GuessingGame
+
+# attr_reader :num_attempts 
+
+def initialize(min, max)
+    # @min.to_i = mini
+    # @max.to_i = maxi
+    
+    # @min.to_i = gets.chomp
+    # @max.to_i = gets.chomp
+
+    @secret_num = rand(@min.to_i..@max.to_i)
+    @num_attempts = 0
+    @game_over = false
+end
+
+def num_attempts
+@num_attempts
+end
+
+def game_over?
+@game_over
+end
+
+
+def check_num (numb)
+numb = numb.to_i
+    if numb == @secret_num
+        puts "game over, you win"
+        game_over = true
+
+    elsif numb != @secret_num && @secret_num > numb
+    puts "too big"
+    @num_attempts += 1
+    elsif numb != @secret_num && @secret_num < numb
+        puts "too small"
+        @num_attempts += 1
+    end
+end
+
+def ask_user
+    puts "enter a number"
+    input = gets
+    num_input.to_i = input.chomp
+    check_num(num_input)
+end
+
+
+
+
+end #last end
